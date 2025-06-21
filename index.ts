f4c41e7b02ed344cfa0883b4df4dd70318feefe4
aeovac <predam@gmx.com>
2025-06-21 04:31:45 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,27 +2,48 @@ import { green, red } from 'kleur';
 import http from 'undici';
 import { guildID, targetID, token, vanity, mfa } from './config';
 import erl from '@typescord/ftee'
+import { dns } from 'bun';
+
+await Promise.allSellted([
+    dns.prefetch('discord.com'),
+    dns.prefetch('gateway.discord.gg'),
+    //Add other potential endpoints
+]);
 
 const client = new WebSocket('wss://gateway.discord.gg/?v=9&encoding=etf');
 
-const dispatcher = new http.Agent({    
-    pipelining: 0,
-    bodyTimeout: 2000,
-    headersTimeout: 2000,
-    keepAliveMaxTimeout: 5000,
-    keepAliveTimeout: 1000,
+const dispatcher = new http.Dispatcher({    
+    pipelining: 15,
+    bodyTimeout: 250,
+    headersTimeout: 150,
+    keepAliveMaxTimeout: 1000,
+    keepAliveTimeout: 250,
+    connect: {
+        rejectUnauthorized: false,
+        socketPath: undefined,
+        tcpNoDelay: true,
+        tcpKeepAlive: true
+    },
+    tls: {
+        sessionTimeout: 0,
+        tiemout: 100
+    }
 });
 
 const headers = { 
     'Authorization': `${token}`,
-    'user-agent': '',
+    'Content-Type': 'application/json',
     'X-Super-Properties': 'ewogICJvcyI6ICJXaW5kb3dzIiwKICAiY2xpZW50X2J1aWxkX251bWJlciI6IDE1MjQ1MAp9',
-    'X-Discord-Mfa-Authorization': mfa
+    'X-Discord-Mfa-Authorization': mfa,
+    'Connection': 'keep-alive',
+    'Keep-alive': 'timeout=5,max=100'
 };
 
 const HEARTBEAT = erl.encode('{"op": 1, "d": null}');
 const IDENTIFY =  erl.encode(`{"op": 2, "d":{"token": "${token}", "intents": 37376,"properties": { "$os":"linux", "$browser":"bun", "$device": "bun"}}}`);
 const BODY =  Buffer.from(`"{ "code": ${vanity} }"`);
+
+Bun.gc(true);
 
 client.onmessage = (({ data }) => {
     const x = erl.decode(data);
@@ -32,11 +53,11 @@ client.onmessage = (({ data }) => {
 
     op == 10
         ? (() => {
-            setInterval(() => client.send(HEARTBEAT), d.heartbeat_interval);
+            setInterval(() => client.send(HEARTBEAT), 30_000);
             client.send(IDENTIFY);
         })()
         : op == 0
-            ? setImmediate(async () => {
+            ? (() => {
                 if(t != "GUILD_UPDATE") return;
                 if(d.guild_id != targetID || d.vanity_url != vanity) return;
                 
@@ -44,11 +65,11 @@ client.onmessage = (({ data }) => {
                     method: 'PATCH',
                     dispatcher,
                     headers,
-                    body: BODY
+                    body: BODY,
                 });
 
-                await body.dump();
                 console.log(statusCode === 200 ? green('Success') : red('Error' + statusCode));     
-            })
+                body.dump().catch(()=>{});
+            })()
             : undefined;
 });
